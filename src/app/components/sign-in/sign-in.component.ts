6a6458cce4d05e568d31253a597794e5cec5e5ad
suruthi-a <suruthianbucheziyan@gmail.com>
2023-06-27 17:53:46 +0530

    username and password validation for sign-in form

--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+ 
 import {PayloadService} from '../../services/payload.service'
 import { IUser, CognitoService } from '../../services/cognito.service';
 import {Amplify, Auth } from 'aws-amplify';
+
 
 @Component({
   selector: 'app-sign-in',
@@ -16,6 +18,12 @@ export class SignInComponent {
   user: IUser;
   isAdmin :boolean;
   signedUser:any;
+  errorMessage: string = '';
+  passwordRequiredErrorMessage: string = 'Password is required';
+  passwordLengthErrorMessage: string = 'Password must be at least 8 characters';
+  EmailRequiredErrorMessage :string = 'Email is required';
+  InvalidEmailErrorMessage: string = 'Invalid Email Address';
+
 
   constructor(private router: Router,
               private cognitoService: CognitoService,private payload:PayloadService,
@@ -24,43 +32,67 @@ export class SignInComponent {
     this.user = {} as IUser;
     this.isAdmin = false;
   }
-  
+
   public signIn(): void {
     this.loading = true;
-    this.cognitoService.signIn(this.user)
-    .then(() => {
-     this.cognitoService.authenticationSubject.next(false);
-      Auth.currentSession()
-    .then((data) => {
-      this.cognitoService.demo();
-    const accessToken = data.getAccessToken().getJwtToken();
-    localStorage.setItem('AccessToken',accessToken );
-    console.log(accessToken)
-    this.payload.GetSignedUserInfo(accessToken).subscribe((res:any)=>{  
-    console.log("signedUser"+ JSON.stringify(res.body)); 
-    const userData=JSON.stringify(res.body); 
-    this.signedUser={
-      userName:res.body.name,
-      profilePic:res.body.profile_pic
-    } 
 
-    this.cognitoService.setSignedUserData(userData);
-    console.log("after insert into service"+this.signedUser);
-    this.isAdmin=res.body.is_admin;
 
-    if(this.isAdmin)
-      {
-        this.router.navigate(['/users']); 
-      }else{
-        this.router.navigate(['/profile']);
-      }
-    });
-  
-  })    
-    }).catch((error) => {
+    if (!this.user.email) {
+      this.errorMessage = this.EmailRequiredErrorMessage;
       this.loading = false;
-      console.error('Error obtaining access token:', error);
-    });
-  }
+      return;
+    } else if (!this.user.email.includes('@') || !this.user.email.includes('.')) {
+        this.errorMessage = this.InvalidEmailErrorMessage;
+        this.loading = false;
+        return;
+      }
 
-}   
+  
+  
+      if (!this.user.password) {
+        this.errorMessage = this.passwordRequiredErrorMessage;
+        this.loading = false;
+        return;
+      } else if (this.user.password.length < 8) {
+        this.errorMessage = this.passwordLengthErrorMessage;
+        this.loading = false;
+        return;
+      }
+    
+  
+    this.cognitoService.signIn(this.user)
+      .then(() => {
+        this.cognitoService.authenticationSubject.next(false);
+        Auth.currentSession().then((data) => {
+          this.cognitoService.demo();
+          const accessToken = data.getAccessToken().getJwtToken();
+          localStorage.setItem('AccessToken', accessToken);
+          console.log(accessToken);
+  
+          this.payload.GetSignedUserInfo(accessToken).subscribe((res: any) => {  
+            console.log('signedUser', JSON.stringify(res.body)); 
+            const userData = JSON.stringify(res.body); 
+            this.signedUser = {
+              userName: res.body.name,
+              profilePic: res.body.profile_pic
+            };
+  
+            this.cognitoService.setSignedUserData(userData);
+            console.log('after insert into service', this.signedUser);
+            this.isAdmin = res.body.is_admin;
+  
+            if (this.isAdmin) {
+              this.router.navigate(['/users']); 
+            } else {
+              this.router.navigate(['/profile']);
+            }
+          });
+        });
+      })
+      .catch((error) => {
+        this.errorMessage = 'Invalid email and password';
+        this.loading = false;
+        console.error('Error obtaining access token:', error);
+      });
+  }
+}
